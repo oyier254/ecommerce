146a2ca54b9557d4505dd4707d1d484e5731bbc6
eugeneoyier <eugeneoyier@outlook.com>
2023-12-03 12:09:48 +0300

    Created shipping model

--- ecommerce/store/models.py
+++ ecommerce/store/models.py
@@ -15,9 +15,11 @@ class Product(models.Model):
     name = models.CharField(max_length=200, null=True)
     price = models.FloatField()
     digital = models.BooleanField(default=False, null=True, blank=False)
+    #image
 
     def __str__(self):
         return self.name
+
 
 class Order(models.Model):
     Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True, blank=True)
@@ -27,4 +29,22 @@ class Order(models.Model):
 
     def __str__(self):
         return str(self.id)
+    
+class OrderItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, blank=True, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, blank=True, null=True)
+    quantity = models.IntegerField(default=0, null=True, blank=True)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+class ShippingAddress(models.Model):
+    Customer = models.ForeignKey(Customer, on_delete=models.SET_NULL, null=True)
+    order = models.ForeignKey(Order, on_delete=models.SET_NULL, null=True)
+    address = models.CharField(max_length=200, null=False)
+    city = models.CharField(max_length=200, null=False)
+    state = models.CharField(max_length=200, null=False)
+    zipcode = models.CharField(max_length=200, null=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.address
     
